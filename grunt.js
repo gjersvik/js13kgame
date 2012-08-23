121e94694fbba1143cc49e238cda6d893b36e6ef
Ole Martin Gjersvik <olem@olem.org>
2012-08-23 17:44:25 +0200

    Remove some html whitespace. Version 1 of my build tools are done.

--- grunt.js
+++ grunt.js
@@ -3,14 +3,16 @@ module.exports = function (grunt) {
     'use strict';
     var zipstream = require('zipstream'),
         cleanCSS = require('clean-css'),
-        fs = require('fs');
+        fs = require('fs'),
+        path = require('path');
 
     // Project configuration.
     grunt.initConfig({
         meta: {
             version: '0.1.0',
             js: ['src/js/*.js'],
-            css: ['src/css/*.css']
+            css: ['src/css/*.css'],
+            index: 'src/index.html'
         },
         concat: {
             js: {
@@ -32,16 +34,58 @@ module.exports = function (grunt) {
             src: ['<config:concat.css.dest>'],
             dest: 'build/m.css'
         },
+        buildHtml: {
+            html: '<config:meta.index>',
+            css: '<config:mincss.dest>',
+            js: '<config:min.js.dest>',
+            dest: 'build/index.html'
+        },
         zip: {
-            src: ['<config:min.js.dest>', '<config:mincss.dest>'],
+            src: ['<config:buildHtml.dest>'],
             dest: 'build/js13kgame.zip',
             map: {from: 'build/', to: ''},
             maxsize: 13312
         },
+        debugHtml: {
+            html: '<config:meta.index>',
+            css: '<config:meta.css>',
+            js: '<config:meta.js>',
+            dest: 'src/debug.html'
+        },
         watch: {
-            files: '<config:meta.js>',
-            tasks: 'concat min mincss zip'
+            files: ['<config:meta.index>', '<config:meta.css>', '<config:meta.js>'],
+            tasks: 'debugHtml concat min mincss buildHtml zip'
         }
+    });
+
+    grunt.registerTask('buildHtml', 'Build html file', function () {
+        this.requiresConfig('buildHtml.html', 'buildHtml.css', 'buildHtml.js', 'buildHtml.dest');
+        var data = {},
+            html = grunt.file.read(grunt.config('buildHtml.html'));
+        data.css = '<style>' + grunt.file.read(grunt.config('buildHtml.css')) + '</style>';
+        data.js = '<script>' + grunt.file.read(grunt.config('buildHtml.js')) + '</script>';
+
+        html = html.replace(/>\s+</g, '><');
+        html = grunt.template.process(html, data);
+        grunt.file.write(grunt.config('buildHtml.dest'), html);
+    });
+
+    grunt.registerTask('debugHtml', 'Build debug version of html file', function () {
+        this.requiresConfig('debugHtml.html', 'debugHtml.css', 'debugHtml.js', 'debugHtml.dest');
+        var data = {css: '', js: ''},
+            htmldir = path.dirname(grunt.config('debugHtml.html')),
+            html = grunt.file.read(grunt.config('debugHtml.html'));
+        grunt.file.expandFiles(grunt.config('debugHtml.css')).forEach(function (file) {
+            data.css += '<link rel="stylesheet" href="' + path.relative(htmldir, file) + '" />\r\n';
+        });
+
+        grunt.file.expandFiles(grunt.config('debugHtml.js')).forEach(function (file) {
+            data.js += '<script src="' + path.relative(htmldir, file) + '"></script>\r\n';
+        });
+
+
+        html = grunt.template.process(html, data);
+        grunt.file.write(grunt.config('debugHtml.dest'), html);
     });
 
     grunt.registerTask('mincss', 'Compresses css files', function () {
@@ -106,6 +150,6 @@ module.exports = function (grunt) {
     });
 
     // Default task.
-    grunt.registerTask('default', 'concat min mincss zip');
+    grunt.registerTask('default', 'debugHtml concat min mincss buildHtml zip');
 
 };
